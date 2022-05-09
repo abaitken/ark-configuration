d688bdb265030eff7d48d35661604cf699850f8f
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-05-09 11:51:52 +0100

    Backup of settings

--- Settings/get_settings.cmd
+++ Settings/get_settings.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-copy /y "C:\Games\ark\ShooterGame\Saved\Config\WindowsServer\*.ini" "Settings\Island\ShooterGame\Saved\Config\WindowsServer\"
-copy /y "C:\Games\arkse\ShooterGame\Saved\Config\WindowsServer\*.ini" "Settings\Scorched\ShooterGame\Saved\Config\WindowsServer\"
+copy /y "C:\Games\ark\ShooterGame\Saved\Config\WindowsServer\*.ini" "Island\ShooterGame\Saved\Config\WindowsServer\"
+copy /y "C:\Games\arkse\ShooterGame\Saved\Config\WindowsServer\*.ini" "Scorched\ShooterGame\Saved\Config\WindowsServer\"
 pause
