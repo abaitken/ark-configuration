232a454b0f71828251613317304d64729da09a8e
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-05-24 18:52:38 +0100

    Added configuration for missing maps and a script to display all settings

---
+++ PrintMapSettings.cmd
@@ -0,0 +1,15 @@
+@echo off
+cd /d "%~dp0"
+
+echo Map,SessionName,Port,QueryPort,RCONPort,AltSaveDirectoryName> temp.txt
+for /f %%i in (maplist.txt) do (
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /f "eol=# tokens=1* delims==" %%j in (args_%%i.txt) do SET %%j=%%k
+echo !map!,!SessionName!,!Port!,!QueryPort!,!RCONPort!,!AltSaveDirectoryName!>> temp.txt
+ENDLOCAL
+)
+
+call ConsoleTable temp.txt
+del temp.txt
+
+pause
---
+++ maps.md
@@ -0,0 +1,19 @@
+# Maps
+
+## Official
+
+ - The Island
+ - Scorched Earth
+ - Aberration
+ - Extinction
+ - Genesis: Part 1
+ - Genesis: Part 2
+
+## Adopted
+
+ - The Center
+ - Ragnarok
+ - Valguero
+ - Crystal Isles
+ - Lost Island
+ - Fjordur
