74a68e7313f35bad63e863fcf09655b2607081a2
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-05-25 17:43:55 +0100

    Moved all map arguments into dedicated folder

--- PrintMapSettings.cmd
+++ PrintMapSettings.cmd
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 echo Map,SessionName,Port,PortRawUDP,QueryPort,RCONPort,AltSaveDirectoryName> temp.txt
 for /f %%i in (maplist.txt) do (
 SETLOCAL ENABLEDELAYEDEXPANSION
-for /f "eol=# tokens=1* delims==" %%j in (args_%%i.txt) do SET %%j=%%k
+for /f "eol=# tokens=1* delims==" %%j in (args\args_%%i.txt) do SET %%j=%%k
 echo !map!,!SessionName!,!Port!,!PortRawUDP!,!QueryPort!,!RCONPort!,!AltSaveDirectoryName!>> temp.txt
 ENDLOCAL
 )
