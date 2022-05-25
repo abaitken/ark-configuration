866f2b00f96a1ce23a28084f367035a008ee7f15
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-05-25 17:38:40 +0100

    Assigned raw port number to variable and added to display

--- PrintMapSettings.cmd
+++ PrintMapSettings.cmd
@@ -1,11 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
-echo Map,SessionName,Port,QueryPort,RCONPort,AltSaveDirectoryName> temp.txt
+echo Map,SessionName,Port,PortRawUDP,QueryPort,RCONPort,AltSaveDirectoryName> temp.txt
 for /f %%i in (maplist.txt) do (
 SETLOCAL ENABLEDELAYEDEXPANSION
 for /f "eol=# tokens=1* delims==" %%j in (args_%%i.txt) do SET %%j=%%k
-echo !map!,!SessionName!,!Port!,!QueryPort!,!RCONPort!,!AltSaveDirectoryName!>> temp.txt
+echo !map!,!SessionName!,!Port!,!PortRawUDP!,!QueryPort!,!RCONPort!,!AltSaveDirectoryName!>> temp.txt
 ENDLOCAL
 )
 
