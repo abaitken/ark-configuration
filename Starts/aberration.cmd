7ecb7e79252470a7c50924e95ec978bc06641a00
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-05-26 19:59:17 +0100

    fix: Name of aberration args file

--- Starts/aberration.cmd
+++ Starts/aberration.cmd
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\_shared.txt) do SET %%i=%%j
-for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\aberation.txt) do SET %%i=%%j
+for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\aberration.txt) do SET %%i=%%j
 for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\_compose.txt) do SET %%i=%%j
 cd /d C:\Games\arkab\ShooterGame\Binaries\Win64
 start ShooterGameServer.exe %Args%
