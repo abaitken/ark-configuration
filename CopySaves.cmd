22db14bab31507540ffb9927a6cc6ebc73283e26
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-11 17:48:25 +0100

    feat: Moved scripts into scripts folder

--- CopySaves.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-copy /y C:\Games\ark\ShooterGame\Saved\Island\TheIsland.ark backups\TheIsland.ark
-copy /y C:\Games\arkse\ShooterGame\Saved\Scorched\ScorchedEarth_P.ark backups\ScorchedEarth_P.ark
-copy /y C:\Games\arkab\ShooterGame\Saved\Aberration\Aberration_P.ark backups\Aberration_P.ark
-copy /y C:\Games\arkex\ShooterGame\Saved\Extinction\Extinction.ark backups\Extinction.ark
-pause
