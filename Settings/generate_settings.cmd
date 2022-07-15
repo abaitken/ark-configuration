bf53fb82567abbea28b0c489b71c36f8103f06f1
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-15 17:40:27 +0100

    feat: Moved settings to backup folder

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -2,8 +2,8 @@
 cd /d "%~dp0"
 
 for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do (
-"merge-ini.exe" -o "%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -m "Template\Shared\Game.txt" -m "Template\%%i\Game.txt"
-"merge-ini.exe" -o "%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -m "Template\Shared\GameUserSettings.txt" -m "Template\%%i\GameUserSettings.txt"
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -m "Template\Shared\Game.txt" -m "Template\%%i\Game.txt"
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -m "Template\Shared\GameUserSettings.txt" -m "Template\%%i\GameUserSettings.txt"
 )
 
 pause
--- Settings/restore_settings.cmd
+++ Settings/restore_settings.cmd
@@ -4,5 +4,5 @@ set /p confirm=Are you sure? [y/N]:
 if /i "%confirm%" NEQ "Y" goto :eof
 cd /d "%~dp0"
 
-for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do if exist "C:\Games\%%j" xcopy /y /E "%%i\ShooterGame\Saved\Config\WindowsServer\" "C:\Games\%%j\ShooterGame\Saved\Config\WindowsServer\"
+for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do if exist "C:\Games\%%j" xcopy /y /E "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\" "C:\Games\%%j\ShooterGame\Saved\Config\WindowsServer\"
 pause
