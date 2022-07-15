5195a6d98b49adedd1fcbb43aa3845fc6f7c6ea4
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-15 18:46:36 +0100

    feat: Implemented server based merge lists

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -3,8 +3,8 @@ cd /d "%~dp0"
 
 for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do (
 echo Building %%i settings...
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\Shared\_mergelist-Game.txt" -m "Template\%%i\Game.txt"
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\Shared\_mergelist-GameUserSettings.txt" -m "Template\%%i\GameUserSettings.txt"
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\Shared\_mergelist-Game.txt" -l "Template\%%i\_mergelist-Game.txt"
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\Shared\_mergelist-GameUserSettings.txt" -l "Template\%%i\_mergelist-GameUserSettings.txt"
 )
 
 pause
