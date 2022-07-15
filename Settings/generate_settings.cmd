9b9b0bffafb54f772deff3f091eaba096700bd4a
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-15 18:29:26 +0100

    feat: Implemented merge lists to help separate configuration files into easier units

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -2,8 +2,9 @@
 cd /d "%~dp0"
 
 for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do (
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -m "Template\Shared\Game.txt" -m "Template\%%i\Game.txt"
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -m "Template\Shared\GameUserSettings.txt" -m "Template\%%i\GameUserSettings.txt"
+echo Building %%i settings...
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\Shared\_mergelist-Game.txt" -m "Template\%%i\Game.txt"
+"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\Shared\_mergelist-GameUserSettings.txt" -m "Template\%%i\GameUserSettings.txt"
 )
 
 pause
