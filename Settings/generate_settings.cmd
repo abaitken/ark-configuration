95d0c3d8ca025f7f7e58b73e15fde2769b70d6e8
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-15 18:59:58 +0100

    feat: Added all maps and fixed generate settings to support missing backup settings

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -1,10 +1,16 @@
 @echo off
+goto :fn_main
+
+:fn_GenerateSetting
+if not exist "Backup\%1" goto :eof
+echo Building %1 settings...
+"merge-ini.exe" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\_Shared\_mergelist-Game.txt" -l "Template\%1\_mergelist-Game.txt"
+"merge-ini.exe" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\_Shared\_mergelist-GameUserSettings.txt" -l "Template\%1\_mergelist-GameUserSettings.txt"
+goto :eof
+
+:fn_main
 cd /d "%~dp0"
 
-for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do (
-echo Building %%i settings...
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\_Shared\_mergelist-Game.txt" -l "Template\%%i\_mergelist-Game.txt"
-"merge-ini.exe" -o "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\_Shared\_mergelist-GameUserSettings.txt" -l "Template\%%i\_mergelist-GameUserSettings.txt"
-)
+for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do call :fn_GenerateSetting %%i
 
 pause
