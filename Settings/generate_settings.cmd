d720d76cdcf14c3ad12f529b48b1586587b9b7af
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-16 13:38:38 +0100

    fix: Fixed script to run on server

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -4,8 +4,8 @@ goto :fn_main
 :fn_GenerateSetting
 if not exist "Backup\%1" goto :eof
 echo Building %1 settings...
-"merge-ini.exe" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\_Shared\_mergelist-Game.txt" -l "Template\%1\_mergelist-Game.txt"
-"merge-ini.exe" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\_Shared\_mergelist-GameUserSettings.txt" -l "Template\%1\_mergelist-GameUserSettings.txt"
+call "merge-ini" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\_Shared\_mergelist-Game.txt" -l "Template\%1\_mergelist-Game.txt"
+call "merge-ini" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\_Shared\_mergelist-GameUserSettings.txt" -l "Template\%1\_mergelist-GameUserSettings.txt"
 goto :eof
 
 :fn_main
