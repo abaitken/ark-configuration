4171ef84e8c60ccc0dfd72db6ff7fc640641829e
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-08-13 23:02:28 +0100

    fix: Fixed issue where game would add an empty line causing different after generating new settings

--- Settings/generate_settings.cmd
+++ Settings/generate_settings.cmd
@@ -5,7 +5,9 @@ goto :fn_main
 if not exist "Backup\%1" goto :eof
 echo Building %1 settings...
 call "merge-ini" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\Game.ini" -l "Template\_Shared\_mergelist-Game.txt" -l "Template\%1\_mergelist-Game.txt"
+echo.>> "Backup\%1\ShooterGame\Saved\Config\WindowsServer\Game.ini"
 call "merge-ini" -o "Backup\%1\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini" -l "Template\_Shared\_mergelist-GameUserSettings.txt" -l "Template\%1\_mergelist-GameUserSettings.txt"
+echo.>> "Backup\%1\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini"
 goto :eof
 
 :fn_main
