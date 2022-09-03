adffe57f5675f36682720ceadf8e898e1cf3fc3f
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-03 18:34:45 +0100

    feat: Renamed from Backup to Staging to accurately represent the purpose of the folder

--- Settings/get_settings.cmd
+++ Settings/get_settings.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do if exist "C:\Games\%%j" copy /y "C:\Games\%%j\ShooterGame\Saved\Config\WindowsServer\*.ini" "Backup\%%i\ShooterGame\Saved\Config\WindowsServer\"
+for /f "eol=# tokens=1,2 delims=," %%i in (list.txt) do if exist "C:\Games\%%j" copy /y "C:\Games\%%j\ShooterGame\Saved\Config\WindowsServer\*.ini" "Staging\%%i\ShooterGame\Saved\Config\WindowsServer\"
 
 pause
