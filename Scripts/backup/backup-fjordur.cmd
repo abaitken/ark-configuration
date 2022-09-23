7d7a6a4bb60b4bb67d1869549fee4bd24452b77b
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-23 23:33:17 +0100

    feat: Added Fjordur configurations and scripts

---
+++ Scripts/backup/backup-fjordur.cmd
@@ -0,0 +1,5 @@
+@echo off
+SET ArgFile=fjordur
+for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\%ArgFile%.txt) do SET %%i=%%j
+call "C:\Data\_backups\backup.cmd" %GameFolder% "C:\Games\%GameFolder%\ShooterGame\Saved\*"
+pause
