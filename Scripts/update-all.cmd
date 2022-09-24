bcb1301f0e874f83f8d6a9c4e0e18bbc1d8ef38b
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-24 16:47:42 +0100

    feat: Altered update script to use generic map list

--- Scripts/update-all.cmd
+++ Scripts/update-all.cmd
@@ -1,16 +1,37 @@
 @echo off
+goto main
+
+:updateGame
+SETLOCAL
+for /f "eol=# tokens=1* delims==" %%i in (%thisscriptdir%..\args\%1.txt) do SET %%i=%%j
+if exist "C:\Games\%GameFolder%" SET GameFolderPresent=true
+if exist "%scriptsdir%%prefix%%GameFolder%.txt" SET UpdateScriptPresent=true
+
+echo.
+echo +++++++++++++++++++++++++++++++++++++++++
+echo Updating %SessionName%...
+echo -----------------------------------------
+if "%GameFolderPresent%" EQU "true" if "%UpdateScriptPresent%" EQU "true" steamcmd +runscript %scriptsdir%%prefix%%GameFolder%.txt
+echo -----------------------------------------
+if "%GameFolderPresent%" NEQ "true" echo WARNING: Game folder not found!
+if "%UpdateScriptPresent%" NEQ "true" echo WARNING: Upgrade script not found!
+echo +++++++++++++++++++++++++++++++++++++++++
+echo.
+ENDLOCAL
+
+goto :eof
+
+:main
+
 set scriptsdir=C:\Control\scripts\
+set thisscriptdir=%~dp0
+set maplist=%thisscriptdir%..\maplist.txt
+set prefix=update_
 
 title Game Server Updates
 call %scriptsdir%_generate_scripts.cmd
 
-set prefix=update_
 cd /d C:\steamcmd
 
-steamcmd +runscript %scriptsdir%%prefix%arkex.txt
-steamcmd +runscript %scriptsdir%%prefix%arkab.txt
-steamcmd +runscript %scriptsdir%%prefix%arkse.txt
-steamcmd +runscript %scriptsdir%%prefix%arkis.txt
-steamcmd +runscript %scriptsdir%%prefix%arkrag.txt
-steamcmd +runscript %scriptsdir%%prefix%arkg1.txt
+for /f "eol=# tokens=1 delims=" %%a in (%maplist%) do call :updateGame %%a
 pause
