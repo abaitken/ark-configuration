4cc58ac5f2d3d4198c25eaa5fafd693132e52ad1
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-10-23 01:03:26 +0100

    feat: Created Genesis 2 scripts

---
+++ Scripts/start/start-genesis2.cmd
@@ -0,0 +1,9 @@
+@echo off
+SET ArgFile=gen2
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\_shared.txt) do SET %%i=%%j
+for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\%ArgFile%.txt) do SET %%i=%%j
+for /f "eol=# tokens=1* delims==" %%i in (C:\Data\ark\args\_compose.txt) do SET %%i=%%j
+cd /d C:\Games\%GameFolder%\ShooterGame\Binaries\Win64
+start ShooterGameServer.exe %Args%
+ENDLOCAL
