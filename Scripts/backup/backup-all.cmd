9f914ae946a83aa3b6376edc70741dfe506d5a5d
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-10-28 00:35:23 +0100

    feat: Improved backup all script to be more dynamic

--- Scripts/backup/backup-all.cmd
+++ Scripts/backup/backup-all.cmd
@@ -1,7 +1,23 @@
 @echo off
 
+goto :main
+
+:backup
+SETLOCAL
+for /f "eol=# tokens=1* delims==" %%i in (%thisscriptdir%\..\..\args\%1.txt) do SET %%i=%%j
+if not exist "C:\Games\%GameFolder%\ShooterGame\Saved\%AltSaveDirectoryName%" goto :eof
+call "C:\Data\_backups\backup.cmd" %GameFolder% "C:\Games\%GameFolder%\ShooterGame\Saved\*"
+ENDLOCAL
+
+goto :eof
+
+:main
+cd /d "%~dp0"
+set thisscriptdir=%cd%
+
 call "C:\Data\_backups\backup.cmd" arkcluster "C:\Data\ark-clusters\*"
-call "C:\Data\_backups\backup.cmd" arkis "C:\Games\ark\ShooterGame\Saved\*"
-call "C:\Data\_backups\backup.cmd" arkab "C:\Games\arkab\ShooterGame\Saved\*"
-call "C:\Data\_backups\backup.cmd" arkse "C:\Games\arkse\ShooterGame\Saved\*"
-call "C:\Data\_backups\backup.cmd" arkex "C:\Games\arkex\ShooterGame\Saved\*"
+
+set maplist=%thisscriptdir%\..\..\maplist.txt
+for /f "eol=# tokens=1 delims=" %%a in (%maplist%) do call :backup %%a
+
+pause
